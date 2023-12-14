536a9e741636e4ed8669edf39a4b2668aaed0587
Cganesh80 <cganaram800@gmail.com>
2023-12-13 20:13:33 -0500

    requirements installed

--- template.py
+++ template.py
@@ -22,7 +22,8 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb", 
+    "templates/index.html"
 
 
 ]
@@ -45,5 +46,4 @@ for filepath in list_of_files:
 
     else:
         logging.info(f"{filename} is already exists")
-        
-                
+
