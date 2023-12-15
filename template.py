a1aa184bbaa72dcdd6b8ac2c58eb620c6b30fc01
Cganesh80 <cganaram800@gmail.com>
2023-12-14 20:01:05 -0500

    data ingestion completed

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
 
 
-project_name="chicken-disease-classifier"
+project_name="chicken_disease_classifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
